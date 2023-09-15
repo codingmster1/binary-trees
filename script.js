5e68bcf728b8cf4d877929fc107abcdd517abc0d
Jeff <doylejeff98@gmail.com>
2023-09-15 10:27:09 -0400

    changed console log for different results

--- script.js
+++ script.js
@@ -214,24 +214,24 @@ class Node {
   
   const bst = new BST();
   
-  bst.add(9);
+  bst.add(2); // change code here for different results within console
   bst.add(4);
-  bst.add(17);
+  bst.add(16);
   bst.add(3);
   bst.add(6);
   bst.add(22);
   bst.add(5);
-  bst.add(7);
-  bst.add(20);
+  bst.add(10);
+  bst.add(25);
   
-  console.log(bst.findMinHeight());
+  console.log(bst.findMinHeight()); // max/min height of treees
   console.log(bst.findMaxHeight());
   console.log(bst.isBalanced());
   bst.add(10);
   console.log(bst.findMinHeight());
   console.log(bst.findMaxHeight());
   console.log(bst.isBalanced());
-  console.log('inOrder: ' + bst.inOrder());
+  console.log('inOrder: ' + bst.inOrder()); 
   console.log('preOrder: ' + bst.preOrder());
   console.log('postOrder: ' + bst.postOrder());
   
